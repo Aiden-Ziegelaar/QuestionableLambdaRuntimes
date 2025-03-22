c421130d8e003fa4206cc98377e29d075b215ee6
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2025-03-22 09:40:29 +0800

    fix: Added compute to ocaml

--- 4_UnifiedRuntime/code/lib/UnifiedRuntime.ml
+++ 4_UnifiedRuntime/code/lib/UnifiedRuntime.ml
@@ -1,4 +1,10 @@
+let remove_vowels s : string =
+  let not_vowel = Fun.negate (String.contains "AaEeIiOoUu") in
+  String.to_seq s |> Seq.filter not_vowel |> String.of_seq
+
 let handler event =
   let name = Yojson.Safe.Util.member "name" event in
   let formatted_string = Yojson.Safe.Util.to_string name in
-  `String ("Hello, " ^ formatted_string ^ "!")
+  let no_vowel_string = remove_vowels formatted_string in
+  `String no_vowel_string
+
