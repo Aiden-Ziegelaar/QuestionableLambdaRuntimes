e617fde9cebd89a3b5189802586b5275dca7cbe3
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2025-03-25 15:21:08 +0800

    fix: Even more functional

--- 4_UnifiedRuntime/code/bin/main.ml
+++ 4_UnifiedRuntime/code/bin/main.ml
@@ -5,8 +5,8 @@ let aws_runtime_api = Sys.getenv "AWS_LAMBDA_RUNTIME_API"
 let runtime_base_url = "http://" ^ aws_runtime_api ^ "/2018-06-01/runtime"
 
 let get_next_invocation () =
-  let uri = Uri.of_string (runtime_base_url ^ "/invocation/next") in
-  Client.get uri >>= fun (resp, body) ->
+  Uri.of_string (runtime_base_url ^ "/invocation/next") |>
+  Client.get >>= fun (resp, body) ->
   Cohttp_lwt.Body.to_string body >>= fun body_str ->
   let headers = Cohttp.Response.headers resp in
   let body_json = Yojson.Safe.from_string body_str in
@@ -15,16 +15,16 @@ let get_next_invocation () =
   | None -> Lwt.fail_with "Missing request ID"
 
 let send_response request_id response_json =
-  let uri = Uri.of_string (runtime_base_url ^ "/invocation/" ^ request_id ^ "/response") in
   let body = Yojson.Safe.to_string response_json in
   let headers = Cohttp.Header.init_with "Content-Type" "application/json" in
-  Client.post ~body:(Cohttp_lwt.Body.of_string body) ~headers uri >>= fun _ ->
+  Uri.of_string (runtime_base_url ^ "/invocation/" ^ request_id ^ "/response") |> 
+  Client.post ~body:(Cohttp_lwt.Body.of_string body) ~headers >>= fun _ ->
   Lwt.return_unit
 
 let rec lambda_loop () =
   get_next_invocation () >>= fun (request_id, event_body) ->
-  let response = UnifiedRuntime.handler event_body in
-  send_response request_id response >>= fun () ->
+  UnifiedRuntime.handler event_body |>
+  send_response request_id >>= fun () ->
   lambda_loop ()
 
 (* Start the runtime *)
